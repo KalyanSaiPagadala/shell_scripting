50295a3312eba016cab732c108bab1dd513104e4
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-02-25 19:20:08 +0530

    shell_script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, i am fine"
+echo "$PERSON2:: how are $PERSON1? "
+echo "$PERSON1:: i am $PERSON2"
