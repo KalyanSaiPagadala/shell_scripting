ff8758bb108640f06200fb39b7ad85db19fc2961
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-02-25 19:35:46 +0530

    shell_script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
+
+echo "please enter the username:"
 read -s USERNAME
+
+echo "username is $USERNAME"  #this is just for reference
+
+echo "please enter the password:"
 read -s PASSWORD
 
-echo "please enter the username$USERNAME:"
 
-echo "username is $USERNAME"
-
-echo "please enter the password:$PASSWORD"
-
-echo "username is $PASSWORD"
+echo "username is $PASSWORD". #this is just for reference
