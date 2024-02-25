32c84756c62744a28ab776ddd42b08bf937ae438
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-02-25 20:05:09 +0530

    shell_script

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -2,6 +2,13 @@
 
 NUMBER1=$1
 NUMBER2=$2
-SUM=$($NUMBER1 + $NUMBER2)
 
-echo "total = $SUM"
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total:: $SUM"
+
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
