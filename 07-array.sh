c70975e6b46f428db72b68bb0b0ef6f1fd114cc6
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-02-26 11:08:33 +0530

    shell_script

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "BERRY" "CHERRY")
+
+echo "first value:: ${FRUITS[0]}"
+
+echo "second value::${FRUITS[1]} "
+
+echo "third value:: ${FRUITS[2]}"
+
+echo "all fruits:: ${FRUITS[@]}"
