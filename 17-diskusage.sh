c5afeef21f0bd2295f683c51e9b90bad107ccc49
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-03-17 11:04:02 +0530

    roboshop conf by shell

---
+++ 17-diskusage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File' |awk '{print $6}'| cut -d % -f1)
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read -r line 
+do 
+    if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
+    then 
+        message+="High Disk Usage on $partition: $usage <br>"
+    fi
+
+done <<< $DISK_USAGE
