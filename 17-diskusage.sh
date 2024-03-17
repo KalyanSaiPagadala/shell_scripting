7327d607cc85d9c39a0982d9064e2d36ae849ead
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2024-03-17 11:17:02 +0530

    roboshop conf by shell

--- 17-diskusage.sh
+++ 17-diskusage.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -vE 'tmp|File' |awk '{print $6}'| cut -d % -f1)
-DISK_THRESHOLD=70
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
 message=""
 
-while IFS= read -r line 
-do 
-    if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
-    then 
-        message+="High Disk Usage on $DISK_USAGE <br>"
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
-
 done <<< $DISK_USAGE
 
 echo -e "Message: $message"
+
+#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
+
+# sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
